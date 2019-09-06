7273e71aa84cd9f30259a0aab52dd87aeda7f3fd
ranjithr <ranjithrs@gmail.com>
2019-09-06 11:21:10 -0700

    fixed unit tests

--- src/test/java/com/jfpipelines/examples/mavensample/CalculatorTest.java
+++ src/test/java/com/jfpipelines/examples/mavensample/CalculatorTest.java
@@ -20,12 +20,12 @@ public class CalculatorTest {
 
 	@Before
 	public void beforeEachTest() {
-		System.out.println("This is executed before each Test");
+		//System.out.println("This is executed before each Test");
 	}
 
 	@After
 	public void afterEachTest() {
-		System.out.println("This is exceuted after each Test");
+		//System.out.println("This is exceuted after each Test");
 	}
 
 	@Test
@@ -51,7 +51,6 @@ public class CalculatorTest {
 		calculator.divison(10, 0);
 	}
 
-	@Ignore
 	@Test
 	public void testEqual() {
 		boolean result = calculator.equalIntegers(20, 20);
